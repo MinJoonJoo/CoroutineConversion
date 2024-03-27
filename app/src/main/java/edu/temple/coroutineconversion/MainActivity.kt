0328cf9515ccd4bc5b434ac76c6ec7bbcee31d73
MinJoon Joo <tuk34896@temple.edu>
2024-03-27 15:44:37 -0400

    deleted and modified coroutines

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,25 +2,18 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+
 import android.widget.Button
 import android.widget.ImageView
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
+
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     lateinit var cakeImageView: ImageView
-    var scope =CoroutineScope(Job()+ Dispatchers.Default)
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,17 +21,13 @@ class MainActivity : AppCompatActivity() {
         cakeImageView = findViewById(R.id.imageView)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            scope.cancel()
-            scope = CoroutineScope(Job() + Dispatchers.Default)
-            scope.launch{
-                countdown()
+            CoroutineScope(Job()+ Dispatchers.Default).launch{
+                repeat(100) {
+                    cakeImageView.alpha = it / 100f
+                    delay(20)
+                }
             }
         }
 
-    }suspend fun countdown(){
-        repeat(100) {
-            handler.sendEmptyMessage(it)
-            delay(20)
-        }
     }
 }
